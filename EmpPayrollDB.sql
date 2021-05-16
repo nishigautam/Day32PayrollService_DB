4de2b948aa26e236e5cf365985c4806349c0c2a5
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-16 17:13:46 +0530

    Ability to create employee payroll table in the database created

--- EmpPayrollDB.sql
+++ EmpPayrollDB.sql
@@ -12,3 +12,23 @@ mysql> SELECT DATABASE();
 | payroll_service |
 +-----------------+
 1 row in set (0.00 sec)
+
+#UC2
+
+mysql> CREATE TABLE employee_payroll
+    -> (
+    -> id       INT unsigned NOT NULL AUTO_INCREMENT,
+    -> name     VARCHAR(50)  NOT NULL,
+    -> salary   DOUBLE       NOT NULL,
+    -> start    DATE         NOT NULL,
+    -> PRIMARY KEY (id)
+    -> );
+Query OK, 0 rows affected (0.11 sec)
+
+mysql> SHOW Tables;
++---------------------------+
+| Tables_in_payroll_service |
++---------------------------+
+| employee_payroll          |
++---------------------------+
+1 row in set (0.01 sec)
