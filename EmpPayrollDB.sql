05e269596e74da9a53d1ed14b5450489778794f3
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-16 17:02:02 +0530

    Ability to create payroll service database

---
+++ EmpPayrollDB.sql
@@ -0,0 +1,14 @@
+#UC1 : Ability to create a payroll service database
+
+mysql> CREATE DATABASE payroll_service;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> USE payroll_service;
+Database changed
+mysql> SELECT DATABASE();
++-----------------+
+| DATABASE()      |
++-----------------+
+| payroll_service |
++-----------------+
+1 row in set (0.00 sec)
